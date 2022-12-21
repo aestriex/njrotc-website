ee1366813b9160b9fe82a921682e9b9138a759b5
aestriex <telantix@gmail.com>
2022-12-20 17:20:46 -0800

    initital commit

---
+++ teams/sailing/script.js
@@ -0,0 +1,73 @@
+$(document).ready(function(){
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 500,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+          $('.warning').addClass("sticky-warning");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing text animation script
+    var typed = new Typed(".typing-2", {
+        strings: ["building confidence", "being a leader", "improving disipline", "learning leadership skills", "learning United States history", "developing mental health skills", "resisting peer pressure", "prioritizing physical fitness"],
+        typeSpeed: 50,
+        shuffle: true,
+        backSpeed: 9,
+        cursorChar: "_",
+        fade: true,
+        color: "navy",
+        eraseDelay: 10000,
+        loop: true
+    });
+
+    // owl carousel script
+});
